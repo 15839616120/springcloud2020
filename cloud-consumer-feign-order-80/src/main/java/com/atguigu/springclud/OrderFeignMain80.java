a9e96f4dd2a7a7ccbfd13c4a4aac98db86fcdf68
wuyz <wyzshiwu@163.com>
2020-10-25 19:45:07 +0800

    截止到四十四个视频

---
+++ cloud-consumer-feign-order-80/src/main/java/com/atguigu/springclud/OrderFeignMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springclud;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class,args);
+    }
+}
