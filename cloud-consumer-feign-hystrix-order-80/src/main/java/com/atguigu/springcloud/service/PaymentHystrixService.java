9e3f4bffa0f21573680c84655ecd391e0ee3208c
wyz <wyzshiwu@163.com>
2020-11-05 08:59:14 +0800

    第五十一个视频

---
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+
+    @GetMapping(value = "/payment/hystrix/ok/{id}")
+    String paymentInfo(Integer id);
+
+    @GetMapping(value = "/payment/hystrix/error/{id}")
+    String paymentInfoError(Integer id);
+}
