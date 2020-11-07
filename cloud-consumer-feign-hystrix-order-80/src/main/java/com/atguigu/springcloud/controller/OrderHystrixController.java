73834d5d0ff670dae7db3655663e2ec638a694d1
wyz <wyzshiwu@163.com>
2020-11-07 15:58:40 +0800

    第五十六个视频

--- cloud-consumer-feign-hystrix-order-80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud.controller;
 
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,8 +41,24 @@ public class OrderHystrixController {
      * @return
      */
     @GetMapping(value = "/consumer/payment/hystrix/error/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfoErrorHandler",
+                    commandProperties = {
+                                          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+                                        })
     public String paymentInfoError(@PathVariable("id") Integer id){
+        //系统错误异常
+        //int a = 10/0;
         String result = paymentHystrixService.paymentInfoError(id);
         return result;
     };
+
+    /**
+     * 兜底方法【服务降级的方法】  超时或者系统异常都会走到这个兜底方法
+     * @param id
+     * @return
+     */
+    public String paymentInfoErrorHandler(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + "  paymentInfoErrorHandler,id: " + id +"我是消费者81,系统繁忙，请稍后重试";
+
+    }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -23,17 +25,35 @@ public class PaymentServiceImpl implements PaymentService {
 
     /**
      * 模拟超时异常
-     *
+     * fallbackMethod 服务降级执行的方法
+     * timeoutInMilliseconds:降级处理超时时间设置
      * @param id
      * @return
      */
     @Override
+    @HystrixCommand(fallbackMethod = "paymentInfoErrorHandler",
+                    commandProperties = {
+                           @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfoError(Integer id) {
+        //异常1
+        //int s = 10/0;
         try {
-            TimeUnit.SECONDS.sleep(3);
+            //异常2
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池： " + Thread.currentThread().getName() + "  paymentInfoError,id: " + id + "\t" + "失败" + " 耗时3秒钟";
+        return "线程池： " + Thread.currentThread().getName() + "  paymentInfoError,id: " + id + "\t"  + " 8001成功啦";
+    }
+
+    /**
+     * 兜底方法【服务降级的方法】  超时或者系统异常都会走到这个兜底方法
+     * @param id
+     * @return
+     */
+    public String paymentInfoErrorHandler(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + "  paymentInfoErrorHandler,id: " + id +"8001系统繁忙，请稍后重试";
+
     }
 }
