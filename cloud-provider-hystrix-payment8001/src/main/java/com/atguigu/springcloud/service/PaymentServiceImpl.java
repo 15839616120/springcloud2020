321167b64e876bb260721660734fac22cd2e68f6
wyz <wyzshiwu@163.com>
2020-11-14 13:11:03 +0800

    第五十八个视频

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentServiceImpl.java
@@ -63,10 +63,10 @@ public class PaymentServiceImpl implements PaymentService {
     @HystrixCommand(
             fallbackMethod = "paymentCircuitBreaker_fallback",
             commandProperties = {
-                    @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),
-                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
-                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
-                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),
+                    @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),//是否开启断路器
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//请求次数
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//时间窗口期
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),//失败率达到多少后跳闸
             }
     )
     public String paymentCircuitBreaker(Integer id){
