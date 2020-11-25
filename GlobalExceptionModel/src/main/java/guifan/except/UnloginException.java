163c13dc0dea6eb5219cc7ca9bde09ab04d40410
wyz <wyzshiwu@163.com>
2020-11-25 10:46:43 +0800

    添加日志处理配置

--- GlobalExceptionModel/src/main/java/guifan/except/UnloginException.java
+++ GlobalExceptionModel/src/main/java/guifan/except/UnloginException.java
@@ -1,4 +1,11 @@
 package guifan.except;
 
 public class UnloginException  extends RuntimeException {
+
+    private String code;
+    private String msg;
+    public UnloginException(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
 }
